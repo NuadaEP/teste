86f49bd60f858f77e06e0f50b01e6fd99aaad4cc
Bruno Cardoso <bruno@liveonsolutions.com>
2020-09-23 11:50:30 -0300

    adicionando HTML, usando CSS

--- arquivo.b1.php
+++ arquivo.b1.php
@@ -1,9 +1,28 @@
-<?php 
-$name = "Daish";
-if ($name=="Bruno"){
-    echo "1";
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        #main{
+            display:flex;
+            justify-content:center;
+            align-items:center;
+        }
+    </style>
+</head>
+<body id="main">
+        <div>
+            <?php 
+                $name = "Daish";
+                if ($name=="Bruno"){
+                    echo "1";
+                }
 
-echo "2";
+                echo "<p>2</p>";
 
-?>
+            ?>
+        </div>
+</body>
+</html>
